a8c666b3e05af6967d618fc64bf8b00b75d1aa56
Marianne K <marianne.hannus@tktk.ee>
2024-10-23 14:17:31 +0300

    add week6stringsandarrays code

---
+++ Week6StringsAndArrays/Program.cs
@@ -0,0 +1,60 @@
+﻿//string hello = "hello, world! here I am! ";
+
+//int stringLength = hello.Length;
+
+
+//hello = hello.Trim();
+
+//int wordCounter = 1;
+
+//for (int i = 0; i < hello.Length; i++)
+//{
+//    if(hello[i] == ' ')
+//    {  
+//        wordCounter++;
+//    }
+//}
+
+//Console.WriteLine($"Word count in {hello}: {wordCounter} words");
+
+//variant2
+//string hello = "hello, world! Here I am! ";
+
+//int stringLength = hello.Length;
+
+
+//hello = hello.Trim();
+
+//int wordCounter = 1;
+
+//for (int i = 0; i < hello.Length; i++)
+//{
+//    if(char.IsWhiteSpace(hello[i]))
+//    {
+//        wordCounter++;
+//    }
+//}
+
+//Console.WriteLine($"Word count in {hello}: {wordCounter} words");
+
+
+string hello = " we are the champions?";
+
+int stringLength = hello.Length;
+
+hello = hello.Trim();
+
+char firstLetter = hello[0];
+Console.WriteLine(firstLetter);
+
+hello = hello.Replace(hello[0], char.ToUpper(hello[0])).Replace('?', '!');
+
+
+firstLetter = hello[0];
+Console.WriteLine(firstLetter);
+
+Console.WriteLine(hello);
+
+
+
+
